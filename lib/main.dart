23e6f76f6227b7571002b978cac06241eb5c0726
janella-may-pangilinan <janellamaypangilinan@gmail.com>
2025-02-19 12:52:30 +0800

    janella

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,22 @@ void main() {
 
 class HomeScreen extends StatelessWidget {
   final List<Map<String, String>> users = [
-    {'name': 'Poul', 'image': 'images/poul.jpg', 'email': 'pouljustinmaninang@gmail.com', 'birthday': 'May 8, 2001', 'contact': '123-456-7890', 'address': 'San luis Pampanga.', 'age': '23'},
+    {'name': 'Poul', 'image': 'images/poul.jpg', 'email': 'pouljustinmaninang@gmail.com', 'birthday': 'May 8, 2001', 'contact': '123-456-7890', 'address': 'San Luis, Pampanga', 'age': '23'},
     {'name': 'Nick', 'image': 'images/nick.jpg', 'email': 'nickrghedelacruz@gmail.com', 'birthday': 'June 20, 1990', 'contact': '234-567-8901', 'address': 'Candaba, Pampanga', 'age': '34'},
     {'name': 'Irish', 'image': 'images/person2.jpg', 'email': 'irishjaneeemagat@gmail.com', 'birthday': 'July 7, 1998', 'contact': '345-678-9012', 'address': 'Candaba, Pampanga', 'age': '26'},
     {'name': 'Janella', 'image': 'images/person1.jpg', 'email': 'janellapangilinan@gmail.com', 'birthday': 'May 27, 1995', 'contact': '456-789-0123', 'address': 'Candaba, Pampanga', 'age': '29'},
     {'name': 'Aira', 'image': 'images/person3.jpg', 'email': 'airamaemagat@gmail.com', 'birthday': 'March 4, 1995', 'contact': '567-890-1234', 'address': 'Candaba, Pampanga', 'age': '29'},
+
   ];
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        middle: Text('Home'),
+        middle: Text('Personal Information'),
         trailing: CupertinoButton(
           padding: EdgeInsets.zero,
-          child: Icon(CupertinoIcons.group, size: 24),
+          child: Icon(CupertinoIcons.info, size: 24),
           onPressed: () {
             showCupertinoDialog(
               context: context,
@@ -45,7 +46,7 @@ class HomeScreen extends StatelessWidget {
                               Container(
                                 decoration: BoxDecoration(
                                   border: Border.all(color: Colors.blue, width: 2),
-                                  borderRadius: BorderRadius.circular(50), // Circular border
+                                  borderRadius: BorderRadius.circular(50),
                                 ),
                                 child: ClipOval(
                                   child: Image.asset(
@@ -79,18 +80,61 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: users.map((user) {
-              return GestureDetector(
-                onTap: () {
-                  showCupertinoDialog(
-                    context: context,
-                    builder: (context) {
-                      return CupertinoAlertDialog(
-                        title: Text(user['name']!),
-                        content: Column(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: users.map((user) {
+                return GestureDetector(
+                  onTap: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoAlertDialog(
+                          title: Text(user['name']!),
+                          content: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.blue, width: 2),
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    user['image']!,
+                                    height: 70,
+                                    width: 70,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text('Email: ${user['email']}'),
+                              Text('Birthday: ${user['birthday']}'),
+                              Text('Age: ${user['age']}'),
+                              Text('Contact: ${user['contact']}'),
+                              Text('Address: ${user['address']}'),
+                            ],
+                          ),
+                          actions: [
+                            CupertinoButton(
+                              child: Text('Close',
+                                  style: TextStyle(color: CupertinoColors.destructiveRed)),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
                           children: [
                             Container(
                               decoration: BoxDecoration(
@@ -100,60 +144,30 @@ class HomeScreen extends StatelessWidget {
                               child: ClipOval(
                                 child: Image.asset(
                                   user['image']!,
-                                  height: 70,
-                                  width: 70,
+                                  height: 50,
+                                  width: 50,
                                   fit: BoxFit.cover,
                                 ),
                               ),
                             ),
-                            SizedBox(height: 10),
-                            Text('Email: ${user['email']}'),
-                            Text('Birthday: ${user['birthday']}'),
-                            Text('Age: ${user['age']}'),
-                            Text('Contact: ${user['contact']}'),
-                            Text('Address: ${user['address']}'),
+                            SizedBox(width: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(user['name']!, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                                Text(user['email']!, style: TextStyle(fontSize: 14, color: Colors.grey[600])),
+                                Text(user['address']!, style: TextStyle(fontSize: 12, color: Colors.grey[700])),
+                              ],
+                            ),
                           ],
                         ),
-                        actions: [
-                          CupertinoButton(
-                            child: Text('Close',
-                                style: TextStyle(color: CupertinoColors.destructiveRed)),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue, width: 2),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: ClipOval(
-                          child: Image.asset(
-                            user['image']!,
-                            height: 50,
-                            width: 50,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Text(user['name']!, style: TextStyle(fontSize: 16)),
-                      SizedBox(width: 10),
-                      Text(user['email']!, style: TextStyle(fontSize: 12)),
-                    ],
+                        Divider(),  // Line between each item
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
+                );
+              }).toList(),
+            ),
           ),
         ),
       ),
